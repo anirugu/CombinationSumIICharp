433a49e469e43ad64bbb86400f2a814a6b93340b
Anirudha Gupta <35772421+anirugu@users.noreply.github.com>
2022-12-23 23:42:12 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CombinationSumIICharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+            sol.CombinationSum2(new int[] { 10, 1, 2, 7, 6, 1, 5 }, 8);
+        }
+    }
+}
+
+public class Solution
+{
+    public IList<IList<int>> CombinationSum2(int[] candidates, int target)
+    {
+        foreach (var item in candidates)
+        {
+            var remain = target - item;
+            candidates.First();
+            
+        }
+        return new List<IList<int>>();
+    }
+}
